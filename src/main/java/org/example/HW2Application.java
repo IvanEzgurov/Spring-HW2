020f39eb598c821902cb2bf03ef3cdc2dee0a8f3
Ivan_Ezgurov <ezgurovivan@gmail.com>
2024-02-12 01:24:45 +0300

    GB - Spring. Homework 2.Tast 1 finish, Task2 start

---
+++ src/main/java/org/example/HW2Application.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HW2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HW2Application.class, args);
+    }
+}
